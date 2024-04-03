fad9ff15f7daec2ff22a098cc8ecbfecb569bb9a
Ahmed Ali <ahmed.ali@hub.com>
2024-04-03 12:34:44 +0500

    atm machine coding

---
+++ index.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000; //dollar
+let myPin = 2244;
+
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pin",
+    message: "enter your pin",
+    type: "number",
+  },
+]);
+if (pinAnswer.pin === myPin) {
+  console.log("Correct Pin Code!!!");
+
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "Please selet one of them",
+      type: "list",
+      choices: ["withdraw", "check balance"],
+    },
+  ]);
+  console.log(operationAns);
+
+  if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number",
+      },
+    ]);
+    // =, -= , +=
+    myBalance -= amountAns.amount;
+
+    console.log("your Remaining balance is " + myBalance);
+  } else if (operationAns.operation === "check balance") {
+    console.log("your balance is: " + myBalance);
+  }
+} else {
+  console.log("Incorrect pin code!! try again ");
+}
